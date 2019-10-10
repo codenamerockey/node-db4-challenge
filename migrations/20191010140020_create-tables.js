706798753fc2dd7552f51bdae32dab2ee778e891
Rockey Simmons <codenamerockey@gmail.com>
2019-10-10 14:51:39 -0700

    created server, added seeds, migrations, and knexfile, db-config file and added all dependencies

---
+++ migrations/20191010140020_create-tables.js
@@ -0,0 +1,29 @@
+exports.up = function(knex) {
+  //1 recipe has many ingredients
+  return knex.schema
+    .createTable('recipies', tbl => {
+      tbl.increments(); //primary key- mantra# 1
+      tbl.string('recipe_name', 128).notNullable();
+      tbl.text('instructions');
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments(); //primary key- mantra# 1
+      tbl.float('quantity');
+
+      // create a foreign key that points to the recipies table
+      tbl
+        .integer('recipe_id')
+        .unsigned() // means our integers must be positive
+        .notNullable() // means the field is required
+        .references('id') //this  indicates which field this actually references
+        .inTable('recipies') // so references and inTable truly set of the foreign key enforcement
+        .onUpdate('CASCADE') //this places contraints on records referenced with foreign keys attached .
+        .onDelete('RESTRICT');
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipies');
+};
